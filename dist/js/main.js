2ba5d106cf381e6acf72b3503060d98794ffdf7b
きくたたくや <gtk.9223@gmail.com>
2022-05-28 08:54:34 +0900

    reserveのアイテム作成中

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,9 @@
 window.onload = function(){
-    
+    let head = document.getElementsByClassName('header')[0].offsetHeight;
+    let reserve = document.getElementById('reserve_body');
+
+    console.log(location.pathname);
+    if( location.pathname === "/reserve/") {
+        reserve.style.marginTop = head + 'px';
+    }
 }
